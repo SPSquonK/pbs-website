b7967ad51881fa2d025ce32589d5e1489341f753
SP SquonK <sp@sqk.fr>
2021-05-30 04:05:09 +0200

    Better task split for pokemons

--- database_builder.js
+++ database_builder.js
@@ -131,10 +131,95 @@ function readPokemon(entries) {
 }
 
 
+class PokemonsBuilder {
+    static make(ini, tm) {
+        return this._pokemons = new PokemonsBuilder(readINIData(ini, readPokemon))
+            .addTMMoves(tm)
+            .applyEvolutionClosure()
+            ._;
+    }
+
+    constructor(initialSet) {
+        this._ = initialSet;
+    }
+
+    static tmContentToTodoList(tmContent) {
+        const lines = tmContent.split("\n");
+
+        const movesToTeach = {};
+
+        let currentMove = undefined;
+        for (const line of lines) {
+            if (line.startsWith("[") && line.endsWith("]")) {
+                currentMove = line.substr(1, line.length - 2);
+            } else {
+                if (currentMove === undefined)
+                    throw Error("Invalid tm.txt file");
+                
+                line.split(",")
+                    .filter(s => s !== '')
+                    .forEach(pokemonInternalName => {
+                        if (movesToTeach[pokemonInternalName] === undefined) {
+                            movesToTeach[pokemonInternalName] = [];
+                        }
+
+                        movesToTeach[pokemonInternalName].push(currentMove);
+                    });
+            }
+        }
+
+        return movesToTeach;
+    }
+
+    static teachMove(pokemon, name, method) {
+        if (pokemon.moves[name] === undefined) {
+            pokemon.moves[name] = [];
+        }
+        
+        pokemon.moves[name].push(method);
+    }
+
+    addTMMoves(content) {
+        const movesToTeach = PokemonsBuilder.tmContentToTodoList(content);
+
+        for (const [pokemonInternalName, moves] of Object.entries(movesToTeach)) {
+            moves.forEach(move => PokemonsBuilder.teachMove(this._[pokemonInternalName], move, "TM"));
+        }
+
+        return this;
+    }
+
+    applyEvolutionClosure() {
+        let stable;
+        do {
+            stable = true;
+
+            for (const pokemon of Object.values(this._)) {
+                let myMoves = Object.values(pokemon.moves);
+
+                for (const evolutionName of pokemon.evolvesInto) {
+                    const evolution = this._[evolutionName];
+
+                    myMoves.forEach(move => {
+                        if (evolution.moves[move] === undefined) {
+                            evolution.moves[move] = ["Preevolution"]
+                            stable = false;
+                        }
+                    })
+                }
+            }
+
+        } while (!stable);
+
+        return this;
+    }
+}
+
+
 module.exports = {
     readCSVMoves: csv => readCSVData(csv, readMove),
     readCSVItems: csv => readCSVData(csv, readItem),
     readCSVAbilities: csv => readCSVData(csv, readAbility),
-    readINIPokemon: ini => readINIData(ini, readPokemon)
+    buildPokemonList: (ini, tms) => PokemonsBuilder.make(ini, tms)
 };
 
--- pokemon.js
+++ pokemon.js
@@ -28,97 +28,6 @@ class Pokemon {
     }
 }
 
-class PokemonsBuilder {
-    static initializeFromIni(iniContent) {
-        return new PokemonsBuilder(DatabaseBuilder.readINIPokemon(iniContent));
-    }
-
-    constructor(initialSet) {
-        this._ = initialSet;
-    }
-
-    static tmContentToTodoList(tmContent) {
-        const lines = tmContent.split("\n");
-
-        const movesToTeach = {};
-
-        let currentMove = undefined;
-        for (const line of lines) {
-            if (line.startsWith("[") && line.endsWith("]")) {
-                currentMove = line.substr(1, line.length - 2);
-            } else {
-                if (currentMove === undefined)
-                    throw Error("Invalid tm.txt file");
-                
-                line.split(",")
-                    .filter(s => s !== '')
-                    .forEach(pokemonInternalName => {
-                        if (movesToTeach[pokemonInternalName] === undefined) {
-                            movesToTeach[pokemonInternalName] = [];
-                        }
-
-                        movesToTeach[pokemonInternalName].push(currentMove);
-                    });
-            }
-        }
-
-        return movesToTeach;
-    }
-
-    static teachMove(pokemon, name, method) {
-        if (pokemon.moves[name] === undefined) {
-            pokemon.moves[name] = [];
-        }
-        
-        pokemon.moves[name].push(method);
-    }
-
-    addTMMoves(content) {
-        const movesToTeach = PokemonsBuilder.tmContentToTodoList(content);
-
-        for (const [pokemonInternalName, moves] of Object.entries(movesToTeach)) {
-            moves.forEach(move => PokemonsBuilder.teachMove(this._[pokemonInternalName], move, "TM"));
-        }
-
-        return this;
-    }
-
-    applyEvolutionClosure() {
-        let stable;
-        do {
-            stable = true;
-
-            for (const pokemon of Object.values(this._)) {
-                let myMoves = Object.values(pokemon.moves);
-
-                for (const evolutionName of pokemon.evolvesInto) {
-                    const evolution = this._[evolutionName];
-
-                    myMoves.forEach(move => {
-                        if (evolution.moves[move] === undefined) {
-                            evolution.moves[move] = ["Preevolution"]
-                            stable = false;
-                        }
-                    })
-                }
-            }
-
-        } while (!stable);
-
-        return this;
-    }
-
-    getPokemons(moves, abilities) {
-        return Object.entries(this._)
-            .reduce(
-                (dict, [id, tempPokemon]) => {
-                    dict[id] = new Pokemon(tempPokemon, moves, abilities)
-                    return dict;
-                }, {}
-            );
-    }
-}
-
 class PBS {
     constructor(pbsDict) {
         this._moves     = DatabaseBuilder.readCSVMoves(pbsDict.moves);
@@ -126,18 +35,17 @@ class PBS {
         
         this._items     = DatabaseBuilder.readCSVItems(pbsDict.items);
         this._abilities = DatabaseBuilder.readCSVAbilities(pbsDict.abilities);
-
-        this._pokemons = PokemonsBuilder.initializeFromIni(pbsDict.pokemon)
-            .addTMMoves(pbsDict.tm)
-            .applyEvolutionClosure()
-            .getPokemons(this._moves, this._abilities);
+        this._pokemons  = Object.entries(DatabaseBuilder.buildPokemonList(pbsDict.pokemon, pbsDict.tm))
+            .reduce(
+                (dict, [id, tempPokemon]) => {
+                    dict[id] = new Pokemon(tempPokemon, this._moves, this._abilities)
+                    return dict;
+                }, {}
+            );
     }
 
     getPokedex() { return this._pokemons; }
-
-
 }
-
 
 module.exports = {
     Pokemon,
